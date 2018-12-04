fe9ee54bcc581e4a2a4ff0c894304649fb071290
James Jones <james.russel.jones@gmail.com>
2018-12-04 18:44:01 -0500

    Initial commit

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mongoUri = Environment.GetEnvironmentVariable("MONGO_URI");
+
+            var settings = MongoClientSettings
+                .FromUrl(MongoUrl.Create(mongoUri));
+
+            var client = new MongoClient(settings);
+
+            var database = client.GetDatabase("SampleDatabase");
+
+            database.CreateCollection("SampleCollection");
+
+            IMongoCollection<SampleBusinessObject> collection = database.GetCollection<SampleBusinessObject>("SampleCollection");
+
+            var newBusinessObject = new SampleBusinessObject()
+            {
+                Id = ObjectId.GenerateNewId(),
+                Name = "Test business object 1"
+            };
+
+            collection.InsertOne(newBusinessObject);
+
+            //var findFilter = new FilterDefinition<SampleBusinessObject>();
+
+            //var fetchedBusinessObject = collection.FindAsync();
+        }
+    }
+}
---
+++ ConsoleApp/SampleBusinessObject.cs
@@ -0,0 +1,11 @@
+﻿using MongoDB.Bson;
+
+namespace ConsoleApp
+{
+    public class SampleBusinessObject
+    {
+        public ObjectId Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
