c78bf5b686f3fff6bd4d87bfab5464282cc257d1
James Jones <james.russel.jones@gmail.com>
2019-10-19 23:10:14 -0400

    Migrate to .NET Core 3.0

---
+++ WebApp/Models/IndexModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class IndexModel
+    {
+        public BusinessObjectItem[] BusinessObjectItems { get; set; }
+
+        public int NumberOfItems { get; set; }
+    }
+}
