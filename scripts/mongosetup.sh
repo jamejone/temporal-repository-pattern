2133942a9e9e228c68f6d6460afdf32c460cb761
James Jones <james.russel.jones@gmail.com>
2019-10-23 23:25:30 -0400

    Replace 3-node replica set with a sharded cluster consisting of three one-node replica sets.

--- scripts/mongosetup.sh
+++ scripts/mongosetup.sh
@@ -2,41 +2,103 @@
 
 sleep 15
 
-echo "Setting up replica set..."
+echo "Setting up the config server..."
 
 echo SETUP.sh time now: `date +"%T" `
+
 mongo --host mongo-primary:27017 <<EOF
    var cfg = {
-        "_id": "rs",
+        "_id": "rs1",
         "version": 1,
         "protocolVersion": NumberLong(1),
-        "members": [
+        configsvr: true,
+		"members": [
             {
                 "_id": 0,
-                "host": "mongo-primary:27017",
-                "priority": 2
-            },
-            {
-                "_id": 1,
-                "host": "mongo-secondary-1:27017",
-                "priority": 0,
-                "tags": {
-                    "secondary": "1"
-                }
-            },
-            {
-                "_id": 2,
-                "host": "mongo-secondary-2:27017",
-                "priority": 0,
-                "tags": {
-                    "secondary": "2"
-                }
+                "host": "mongo-primary:27017"
             }
         ]
     };
     rs.initiate(cfg, { force: true });
     rs.reconfig(cfg, { force: true });
-    db.getMongo().setReadPref('primaryPreferred');
+EOF
+
+sleep 15
+
+echo "Setting up mongo-secondary-2..."
+
+echo SETUP.sh time now: `date +"%T" `
+
+mongo --host mongo-secondary-2:27017 <<EOF
+   var cfg = {
+        "_id": "rs2",
+        "version": 1,
+        "protocolVersion": NumberLong(1),
+		"members": [
+            {
+                "_id": 0,
+                "host": "mongo-secondary-2:27017"
+            }
+        ]
+    };
+    rs.initiate(cfg, { force: true });
+    rs.reconfig(cfg, { force: true });
+EOF
+
+
+
+echo "Setting up mongo-secondary-3..."
+
+echo SETUP.sh time now: `date +"%T" `
+
+mongo --host mongo-secondary-3:27017 <<EOF
+   var cfg = {
+        "_id": "rs3",
+        "version": 1,
+        "protocolVersion": NumberLong(1),
+		"members": [
+            {
+                "_id": 0,
+                "host": "mongo-secondary-3:27017"
+            }
+        ]
+    };
+    rs.initiate(cfg, { force: true });
+    rs.reconfig(cfg, { force: true });
+EOF
+
+
+
+echo "Setting up mongo-secondary-4..."
+
+echo SETUP.sh time now: `date +"%T" `
+
+mongo --host mongo-secondary-4:27017 <<EOF
+   var cfg = {
+        "_id": "rs4",
+        "version": 1,
+        "protocolVersion": NumberLong(1),
+		"members": [
+            {
+                "_id": 0,
+                "host": "mongo-secondary-4:27017"
+            }
+        ]
+    };
+    rs.initiate(cfg, { force: true });
+    rs.reconfig(cfg, { force: true });
+EOF
+
+
+
+sleep 15
+
+echo "Setting up the mongos server..."
+
+mongo --host mongo-secondary-1:27017 <<EOF
+   sh.addShard("rs2/mongo-secondary-2:27017");
+   sh.addShard("rs3/mongo-secondary-3:27017");
+   sh.addShard("rs4/mongo-secondary-4:27017");
 EOF
 
 tail -f /dev/null
